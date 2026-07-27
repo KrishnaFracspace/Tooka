dfda001d8cb1f216669330e267394794d74af9f8
KrishnaFracspace <krishna@fracspace.com>
2026-07-27 10:25:47 +0530

    OTA Completed

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Toast from 'react-native-toast-message';
-import { restart, sync, useStallionUpdate, withStallion } from 'react-native-stallion';
+import { ACTIVE_RELEASE_HASH, addEventListener, removeEventListener, restart, sync, useStallionUpdate, withStallion } from 'react-native-stallion';
 
 import AppNavigator from './src/navigation/AppNavigator';
 import { AuthProvider } from './src/context/AuthContext';
@@ -13,12 +13,24 @@ import { ProfileProvider } from './src/context/ProfileContext';
 import { PaymentProvider } from './src/context/PaymentContext';
 
 function App() {
-  const { isRestartRequired, currentlyRunningBundle } = useStallionUpdate();
+  const { isRestartRequired, currentlyRunningBundle, newReleaseBundle } = useStallionUpdate();
 
   // Check for OTA updates when the app starts
   useEffect(() => {
-    sync();
+    if (!__DEV__) {
+      sync();
+    }
   }, []);
+
+  useEffect(() => {
+    console.log('=== Stallion Debug ===');
+    console.log('isRestartRequired:', isRestartRequired);
+    if (!__DEV__ && isRestartRequired) {
+      // console.log('newReleaseBundle:', newReleaseBundle);
+      console.log('currentlyRunningBundle:', currentlyRunningBundle); 
+      restart();
+    }
+  }, [isRestartRequired]);
 
   // Restart automatically once a new bundle has been downloaded
   // useEffect(() => {
