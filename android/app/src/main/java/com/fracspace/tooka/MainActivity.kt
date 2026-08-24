75af77a3fd3253856fcf560d93ff25e04022e907
KrishnaFracspace <krishna@fracspace.com>
2026-08-24 10:06:26 +0530

    Fixed segment crash

--- android/app/src/main/java/com/fracspace/tooka/MainActivity.kt
+++ android/app/src/main/java/com/fracspace/tooka/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.fracspace.tooka
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -8,14 +9,24 @@ import com.facebook.react.defaults.DefaultReactActivityDelegate
 class MainActivity : ReactActivity() {
 
   /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
+   * Returns the name of the main component registered from JavaScript.
    */
   override fun getMainComponentName(): String = "Tooka"
 
   /**
-   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
-   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+   * Prevent Android from restoring the old Fragment state.
+   *
+   * react-native-screens manages its own screen fragments and
+   * restoring them through Android's FragmentManager can cause:
+   *
+   * "Screen fragments should never be restored."
+   */
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+  }
+
+  /**
+   * Returns the instance of the ReactActivityDelegate.
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
